1f92ef376e90d2a17dfa36da648779582581836c
Dennis <30983115+DennisB66@users.noreply.github.com>
2022-04-20 20:48:48 +0200

    small corrections

--- custom_components/buienradar_precipitation_forecast/__init__.py
+++ custom_components/buienradar_precipitation_forecast/__init__.py
@@ -1,12 +1,8 @@
-"""
-Custom integration to integrate integration_blueprint with Home Assistant.
-
-For more details about this integration, please refer to
-https://github.com/custom-components/integration_blueprint
-"""
-import asyncio
 from datetime import timedelta
+import asyncio
 import logging
+
+_LOGGER: logging.Logger = logging.getLogger(__package__)
 
 from homeassistant.const import CONF_NAME, CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.config_entries import ConfigEntry
@@ -16,20 +12,17 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .api import ForecastApiClient
-
 from .const import DOMAIN, PLATFORMS, STARTUP_MESSAGE
 
-SCAN_INTERVAL = timedelta(seconds=30)
-
-_LOGGER: logging.Logger = logging.getLogger(__package__)
+SCAN_INTERVAL = timedelta(seconds=300)
 
 
 async def async_setup(hass: HomeAssistant, config: Config):
     """Set up this integration using YAML is not supported."""
     return True
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up this integration using UI."""
     if hass.data.get(DOMAIN) is None:
         hass.data.setdefault(DOMAIN, {})
         _LOGGER.info(STARTUP_MESSAGE)
@@ -50,24 +43,22 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     for platform in PLATFORMS:
         coordinator.platforms.append(platform)
-        hass.async_add_job(hass.config_entries.async_forward_entry_setup(entry, platform))
+        hass.async_add_job(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
 
 class ForecastDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching data from the API."""
-
     def __init__(self, hass: HomeAssistant, client: ForecastApiClient) -> None:
-        """Initialize."""
         self.api = client
         self.platforms = []
 
         super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=SCAN_INTERVAL)
 
     async def _async_update_data(self):
-        """Update data via library."""
         try:
             return await self.api.async_get_data()
         except Exception as exception:
@@ -75,7 +66,6 @@ class ForecastDataUpdateCoordinator(DataUpdateCoordinator):
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Handle removal of an entry."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
     unloaded = all(
         await asyncio.gather(
@@ -93,6 +83,5 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
--- custom_components/buienradar_precipitation_forecast/config_flow.py
+++ custom_components/buienradar_precipitation_forecast/config_flow.py
@@ -9,6 +9,7 @@ from .const import DOMAIN, PLATFORMS
 
 import voluptuous as vol
 
+
 class ForecastFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
@@ -23,32 +24,35 @@ class ForecastFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         # if self._async_current_entries():
         #     return self.async_abort(reason="single_instance_allowed")
 
-        if user_input is not None:
-            valid = await self._is_valid(user_input)
+        if user_input is None:
+            user_input = {}
+            user_input[CONF_NAME] = self.hass.config.location_name
+            user_input[CONF_LATITUDE] = self.hass.config.latitude
+            user_input[CONF_LONGITUDE] = self.hass.config.longitude
 
-            if valid:
-                return self.async_create_entry(
-                    title=user_input[CONF_NAME], data=user_input
-                )
-            else:
-                return await self._show_config_form(user_input)
+            return await self._show_config_form(user_input)
 
-        user_input = {}
-        user_input[CONF_NAME]      = self.hass.config.location_name
-        user_input[CONF_LATITUDE]  = self.hass.config.latitude
-        user_input[CONF_LONGITUDE] = self.hass.config.longitude
+        if not await self._is_valid(user_input):
+            return await self._show_config_form(user_input)
 
-        return await self._show_config_form(user_input)
+        return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
     async def _show_config_form(self, user_input):
-        data_schema = vol.Schema({
-                vol.Required(CONF_NAME     , default=user_input[CONF_NAME])     : str,
-                vol.Required(CONF_LATITUDE , default=user_input[CONF_LATITUDE]) : cv.latitude,
-                vol.Required(CONF_LONGITUDE, default=user_input[CONF_LONGITUDE]): cv.longitude,
-        })
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_NAME, default=user_input[CONF_NAME]): str,
+                vol.Required(
+                    CONF_LATITUDE, default=user_input[CONF_LATITUDE]
+                ): cv.latitude,
+                vol.Required(
+                    CONF_LONGITUDE, default=user_input[CONF_LONGITUDE]
+                ): cv.longitude,
+            }
+        )
 
         return self.async_show_form(
-            step_id="user", data_schema=data_schema, errors=self._errors)
+            step_id="user", data_schema=data_schema, errors=self._errors
+        )
 
     async def _is_valid(self, user_input):
         # todo: validate user input
@@ -61,7 +65,6 @@ class ForecastFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
 
 class ForecastOptionsFlowHandler(config_entries.OptionsFlow):
-
     def __init__(self, config_entry):
         self.config_entry = config_entry
         self.options = dict(config_entry.options)
@@ -74,14 +77,16 @@ class ForecastOptionsFlowHandler(config_entries.OptionsFlow):
             self.options.update(user_input)
             return await self._update_options()
 
-        data_schema = vol.Schema({
+        data_schema = vol.Schema(
+            {
                 vol.Required(x, default=self.options.get(x, True)): bool
                 for x in sorted(PLATFORMS)
-        })
+            }
+        )
 
         return self.async_show_form(step_id="user", data_schema=data_schema)
 
     async def _update_options(self):
         return self.async_create_entry(
-            title=self.config_entry.data.get(CONF_USERNAME), data=self.options
+            title=self.config_entry.data.get(CONF_NAME), data=self.options
         )
